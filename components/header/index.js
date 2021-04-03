25ee5c2efe7c65d6faeca9dc429cbfc908f120fe
jai-sharma <sendtojsharma@gmail.com>
2021-04-03 19:14:47 +0530

    added header

---
+++ components/header/index.js
@@ -0,0 +1,20 @@
+import { Button } from '@chakra-ui/button';
+import { Box, Flex, Text } from '@chakra-ui/layout';
+
+export default function Header() {
+    return (
+        <Flex as="header" background="gray.100" h="120" alignItems="flex-end">
+            <Flex maxW="88%" w="100%" mx="auto" mb="4" alignItems="center">
+                <Box mr="auto">
+                    <Text fontSize="3xl">
+                        NextBoards
+                    </Text>
+                    <Text>Next Gen Moodboards</Text>
+                </Box>
+                <Button colorScheme="blue" variant="outline">
+                    Create
+                </Button>
+            </Flex>
+        </Flex>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import NextHead from 'components/next-head';
+import Header from 'components/header';
 
 export default function Home() {
     return (
         <>
             <NextHead title="Nextboards - Next Gen Moodboards."/>
         
-           
+           <Header/>
         </>
     );
 }
